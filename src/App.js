55165e0b1ab84668954343c11d8601612fa80081
gabi0702 <gabi.nakache2015@gmail.com>
2023-03-08 16:29:52 +0200

    BlackJack-Blockchain

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { Box, Typography } from "@material-ui/core";
+import BlackJack from "./BlackJack";
+import React from "react";
+import { useEffect } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      {/* <Typography variant="h1">
+        <Box className={"h1-header"}>BlackJack</Box>
+      </Typography> */}
+      <Box id="table">
+        <BlackJack />
+      </Box>
+    </Box>
   );
 }
-
-export default App;
